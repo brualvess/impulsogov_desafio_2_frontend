bf546b039412af6d9ba635eefc6410ceaa7bfe00
brualvess <oliveirabrunna2210@gmail.com>
2023-01-04 17:34:40 -0300

    firts commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// }
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const withTM = require('next-transpile-modules')(['@impulsogov/design-system']);
+
+module.exports = withTM({
+  reactStrictMode: true,
+});
