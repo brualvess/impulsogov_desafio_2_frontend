a54a043670aa35dc1c2cc42e6ad71ac6e7687ac7
brualvess <oliveirabrunna2210@gmail.com>
2023-01-06 19:21:54 -0300

    logo

--- pages/components/Logo.js
+++ pages/components/Logo.js
@@ -3,12 +3,14 @@ import Modal1 from '../../public/images/logo.svg'
 import Modal2 from '../../public/images/logoo.svg'
 import styles from '../../styles/Logo.module.css'
 
-export default function Logo (){
-    return(
-        <logo className={styles.logo}>
-        <Image src={Modal1} alt="" />
-        <Image src={Modal2} alt="" />
-        <Image src={Modal2} alt="" />
-    </logo>
+
+export default function Logo(props) {
+    
+    return (
+        <logo className={styles.logo} onClick={() => props.setShowModal(true)}>
+            <Image src={Modal1} alt="" />
+            <Image src={Modal2} alt="" />
+            <Image src={Modal2} alt="" />
+        </logo>
     )
 }
